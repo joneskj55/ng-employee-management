793481313f0f59444253fc9d1653e1b28d531708
joneskj55 <joneskj55@gmail.com>
2021-04-15 13:57:42 -0500

    add mongoose, connect to db

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+
+// "mongodb://localhost:27017/test"
+
+var mongoDB = "mongodb://localhost:27017/test";
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connected error: "));
+db.once("open", function () {
+  console.log("Application connected to mLab MongoDB instance");
+});
 
 var app = express();
 
