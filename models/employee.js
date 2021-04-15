662cc5a7a1598f627bd836122d3ebcdc7dc8d848
joneskj55 <joneskj55@gmail.com>
2021-04-15 13:44:49 -0500

    create employee model

---
+++ models/employee.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Employee Schema
+let employeeSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  position: { type: String, required: true },
+  department: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  salary: { type: String, required: true },
+});
+
+// Export the model; Define the model
+module.exports = mongoose.model("Employee", employeeSchema);
