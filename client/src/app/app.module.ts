366d5b3cd648c13bd60795bca50ae909f7977626
joneskj55 <joneskj55@gmail.com>
2021-04-18 12:32:30 -0500

    add angular routing (doesn't work)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 
+const routes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+];
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ListComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
