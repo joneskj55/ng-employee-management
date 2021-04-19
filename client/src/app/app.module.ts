6d72b96bffa7598666cee10ccb1d9fdb710c2397
joneskj55 <joneskj55@gmail.com>
2021-04-19 08:16:42 -0500

    new & list view routing complete

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,15 +5,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { HomeComponent } from './home/home.component';
+import { NewComponent } from './new/new.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'list', component: ListComponent },
+  { path: 'new', component: NewComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
-  declarations: [AppComponent, ListComponent, HomeComponent],
+  declarations: [AppComponent, ListComponent, HomeComponent, NewComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
